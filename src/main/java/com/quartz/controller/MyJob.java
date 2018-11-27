9fcfd5f43cc61876b31c2b97436c8b6bb05693eb
tcaironglnan <995956062@qq.com>
2018-11-27 10:52:24 +0800

    1.初步完善

--- src/main/java/com/quartz/controller/MyJob.java
+++ src/main/java/com/quartz/controller/MyJob.java
@@ -1,18 +1,19 @@
 package com.quartz.controller;
 
-import java.util.Date;
-
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
+ * 业务执行类
  * @author FeeMo
  */
 public class MyJob implements Job{
-
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println(new Date() + ": doing something...");
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " ****************** doing something...");
     }
 }
--- src/main/test/QuartzTest.java
+++ src/main/test/QuartzTest.java
@@ -8,7 +8,7 @@ import com.quartz.controller.QuartzManager;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = "classpath:spring-*.xml")
-public class UserTest {
+public class QuartzTest {
 
     public static String JOB_NAME = "动态任务调度";
     public static String TRIGGER_NAME = "动态任务触发器";
@@ -23,8 +23,8 @@ public class UserTest {
             QuartzManager.addJob(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, MyJob.class, "0/1 * * * * ?");
 
 //            Thread.sleep(5000);
-            System.out.println("【修改时间】开始(每5秒输出一次)...");
-            QuartzManager.modifyJobTime(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, "0/5 * * * * ?");
+//            System.out.println("【修改时间】开始(每5秒输出一次)...");
+//            QuartzManager.modifyJobTime(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, "0/5 * * * * ?");
 
 //            Thread.sleep(6000);
             System.out.println("【移除定时】开始...");
